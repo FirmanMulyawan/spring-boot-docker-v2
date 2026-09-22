4dce670b8e46d36bbfbbdd1a13ba59f51d94e027
FirmanMulyawan <firmanmulyawan491@gmail.com>
2026-09-22 10:52:55 +0700

    add default spring boot

---
+++ belajar-spring-docker-v2/src/main/java/com/example/belajar_spring_docker_v2/BelajarSpringDockerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_docker_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringDockerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringDockerV2Application.class, args);
+	}
+
+}
---
+++ belajar-spring-docker-v2/src/test/java/com/example/belajar_spring_docker_v2/BelajarSpringDockerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_docker_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDockerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
